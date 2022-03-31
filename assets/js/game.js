27ee7ea504f847967402e584b81f288b06297015
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-03-30 18:20:28 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,9 +71,22 @@ var fight = function(enemyName) {
 
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+if (playerHealth > 0) {
+  window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+
+    var pickedEnemyName = enemyNames[i];
+
+    enemyHealth = 50;
+
+    fight(pickedEnemyName);
+} else {
+  window.alert("You have lost your robot in battle! Game Over!");
+  break;
 }
+
+//   for(var i = 0; i < enemyNames.length; i++) {
+//   var pickedEnemyName = enemyNames[i];
+//   enemyHealth = 50;
+//   fight(pickedEnemyName);
+// }
  
