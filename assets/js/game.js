09991039cdd2a218dada6506965337b62ebe2b83
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-03 19:29:15 -0700

    fixed name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -177,8 +177,19 @@ var randomNumber = function(min, max) {
 }
 
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name!?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10, 
   money: 10,
