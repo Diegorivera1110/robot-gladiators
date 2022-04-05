90e8137c886e3309450613998dfbf7e27902239d
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-04 19:20:06 -0700

    added local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,12 +142,22 @@ if (playerInfo.health > 0) {
 
 
 var endGame = function() {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  //check local storage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   }
   else {
-  window.alert("You've lost your robot in battle.");
+  alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
 var playAgainConfirm = window.confirm("Would you like to play again?");
